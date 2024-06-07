c25b95a1107f2d3d96cd379294adfca237b1616b
Douglas Bitencourt de Oliveira <douglasoliveirab11@gmail.com>
2024-06-06 21:32:55 -0300

    adicionado espacamento nos metodos

--- web/models/Candidato.php
+++ web/models/Candidato.php
@@ -44,33 +44,40 @@ class Candidato
     {
         return $this->id;
     }
+
     public function setId($id)
     {
         $this->id = $id;
         return $this;
     }
+
     public function getNome()
     {
         return $this->nome;
     }
+
     public function setNome($nome)
     {
         $this->nome = $nome;
         return $this;
     }
+
     public function getEmail()
     {
         return $this->email;
     }
+
     public function setEmail($email)
     {
         $this->email = $email;
         return $this;
     }
+
     public function getSenha()
     {
         return $this->senha;
     }
+
     public function setSenha($senha)
     {
         $this->senha = $senha;
@@ -81,69 +88,84 @@ class Candidato
     {
         return $this->habilidades;
     }
+
     public function setHabilidades($habilidades)
     {
         $this->habilidades = $habilidades;
         return $this;
     }
+
     public function getCpf()
     {
         return $this->cpf;
     }
+
     public function setCpf($cpf)
     {
         $this->cpf = $cpf;
         return $this;
     }
+
     public function getNascimento()
     {
         return $this->nascimento;
     }
+
     public function setNascimento($nascimento)
     {
         $this->nascimento = $nascimento;
         return $this;
     }
+
     public function getEndereco()
     {
         return $this->endereco;
     }
+
     public function setEndereco($endereco)
     {
         $this->endereco = $endereco;
         return $this;
     }
+
     public function getDisponibilidade()
     {
         return $this->disponibilidade;
     }
+
     public function setDisponibilidade($disponibilidade)
     {
         $this->disponibilidade = $disponibilidade;
         return $this;
     }
+
     public function getSexo()
     {
         return $this->sexo;
     }
+
     public function setSexo($sexo)
     {
         $this->sexo = $sexo;
         return $this;
     }
+
     public function getGenero()
     {
         return $this->genero;
     }
+
     public function setGenero($genero)
     {
         $this->genero = $genero;
         return $this;
     }
+
     public function getStatus()
     {
         return $this->status;
     }
+
     public function SetStatus($status)
     {
         $this->status = $status;
