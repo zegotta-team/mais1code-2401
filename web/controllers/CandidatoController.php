b9ecb601cce7574feb30bcd20faa3a6a9250b201
emg_srv4 <emgsrv4@hportugues.com.br>
2024-06-15 10:12:09 -0300

    adiçao de retorno para html quando cadastro for realizado com sucesso

--- web/controllers/CandidatoController.php
+++ web/controllers/CandidatoController.php
@@ -42,8 +42,8 @@ class CandidatoController
 
         $candidato = new Candidato($_POST['nome'], $_POST['email'], $_POST['senha'], $_POST['habilidades'], $_POST['cpf'], $_POST['nascimento'], $_POST['endereco'], $_POST['disponibilidade'], $_POST['sexo'], $_POST['genero'], $_POST['status']);
         
-        $retorno = CandidatoDTO::salvar($candidato);
-        echo "$retorno";
-        header('Location: /');
+        CandidatoDTO::salvar($candidato);
+        $_SESSION['CadastroRealizado'] = 'Usuário/candidato cadastrado com sucesso';
+        header('Location: /candidato/login');
     }
 }
