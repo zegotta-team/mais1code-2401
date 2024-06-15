3800e292a3596e0ed7fac58b1eaeeae98fa24c46
Douglas Bitencourt de Oliveira <douglasoliveirab11@gmail.com>
2024-06-15 15:00:41 -0300

    alterado painel de vagas quando candidato logado

--- web/controllers/CandidatoController.php
+++ web/controllers/CandidatoController.php
@@ -41,7 +41,7 @@ class CandidatoController
         AutenticacaoController::renegaSessao();
 
         $candidato = new Candidato($_POST['nome'], $_POST['email'], $_POST['senha'], $_POST['habilidades'], $_POST['cpf'], $_POST['nascimento'], $_POST['endereco'], $_POST['disponibilidade'], $_POST['sexo'], $_POST['genero'], $_POST['status']);
-        
+
         CandidatoDTO::salvar($candidato);
         $_SESSION['CadastroRealizado'] = 'Usuário/candidato cadastrado com sucesso';
         header('Location: /candidato/login');
