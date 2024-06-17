b11b358d0331f5003ce0d3fe23575a03024e229e
Thiago Abreu <thiago.abreu@i9xp.com.br>
2024-06-16 21:31:40 -0300

    Adicionado hasheamento de senha de usuario

--- web/controllers/EmpresaController.php
+++ web/controllers/EmpresaController.php
@@ -21,7 +21,7 @@ class EmpresaController
         $empresa = new Empresa($_POST['nome'], $_POST['cnpj'], $_POST['email'], $_POST['descricao'], $_POST['logo'], $_POST['endereco']);
         EmpresaDTO::salvar($empresa);
 
-        $usuario = new Usuario($empresa, $_POST['usuarioCpf'], $_POST['usuarioNome'], $_POST['usuarioEmail'], $_POST['usuarioSenha']);
+        $usuario = new Usuario($empresa, $_POST['usuarioCpf'], $_POST['usuarioNome'], $_POST['usuarioEmail'], password_hash($_POST['usuarioSenha'], PASSWORD_ARGON2ID));
         UsuarioDTO::salvar($usuario);
 
         header('Location: /autenticacao');
