d4d3448a1aa12b782d082eb9f8507e5e06f31b5b
Edyelgue Carneiro <edyelgue12@gmail.com>
2024-06-22 01:06:27 -0300

    Criado CandidatoVagaDTO

--- web/models/CandidatoVagaDTO.php
+++ web/models/CandidatoVagaDTO.php
@@ -8,7 +8,55 @@ abstract class CandidatoVagaDTO implements DTOInterface
         $candidato = CandidatoDTO::recuperar($dados['candidato_id']);
         $vaga = VagaDTO::recuperar($dados['vaga_id']);
         $candidatoVaga = new CandidatoVaga($candidato, $vaga, $dados['ultima_desistencia'], $dados['status']);
-
+        
         return $candidatoVaga;
+    }
+
+    public static function salvar($candidatoVaga)
+    {
+        $pdo = static::conectarDB();
+
+        if (empty($candidatoVaga->getCandidato()->getId() && $candidatoVaga->getVaga()->getId())) {
+            $sql = "INSERT INTO candidato_vaga (candidato_id, vaga_id, ultima_desistencia, `status`) VALUES ({$candidatoVaga->getCandidato()->getId()}, {$candidatoVaga->getVaga()->getId()}, \"{$candidatoVaga->getUltimaDesistencia()}\", \"{$candidatoVaga->getStatus()}\")";
+        } else {
+            $sql = "UPDATE candidato_vaga SET ultima_desistencia = '{$candidatoVaga->getUltimaDesistencia()}', '{{$candidatoVaga->getStatus()}' WHERE candidato_id = '{$candidatoVaga->getCandidato()->getId()}' AND vaga_id = '{$candidatoVaga->getVaga()->getId()}'";
+        }
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+    }
+
+    public static function recuperar($candidato_id)
+    {
+        $pdo = static::conectarDB();
+        $sql = "SELECT * FROM candidato_vaga WHERE candidato_id = $candidato_id";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+
+        $retorno = null;
+        while ($candidatoVaga = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $retorno = static::preecher($candidatoVaga);
+        }
+
+        return $retorno;
+    }
+
+    public static function listar($candidato_id = '')
+    {
+        $pdo = static::conectarDB();
+        $sql = "SELECT candidato_vaga.candidato_id, vaga.* 
+                FROM candidato_vaga 
+                INNER JOIN vaga ON id  = candidato_vaga.vaga_id 
+                WHERE candidato_vaga.candidato_id = $candidato_id";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+
+        $retorno = [];
+        while ($candidatoVaga = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $retorno[] = static::preecher($candidatoVaga);
+        }
+        return $retorno;
     }
 }
