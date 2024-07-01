90ba6aeb20c3552b298fe998cbf1b9b524eca278
Hitman Gry <kauan.silvanascimento@outlook.com>
2024-07-01 15:32:27 -0300

    Adição da camada de modelo das filiais

---
+++ web/models/Filial.php
@@ -0,0 +1,141 @@
+<?php
+class Filial
+{
+
+    private $id;
+    private Empresa $empresa;
+    private $nome;
+    private $cep;
+    private $logradouro;
+    private $numero;
+    private $complemento;
+    private $bairro;
+    private $cidade;
+    private $estado;
+    
+    public function __construct(Empresa $empresa, $nome, $cep, $logradouro, $numero, $complemento, $bairro, $cidade, $estado)
+    {
+        $this->setEmpresa($empresa);
+        $this->setNome($nome);
+        $this->setCep($cep);
+        $this->setLogradouro($logradouro);
+        $this->setNumero($numero);
+        $this->setComplemento($complemento);
+        $this->setBairro($bairro);
+        $this->setCidade($cidade);
+        $this->setEstado($estado);
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+    
+    public function getEmpresa(): Empresa
+    {
+        return $this->empresa;
+    }
+
+    public function setEmpresa(Empresa $empresa): Filial
+    {
+        $this->empresa = $empresa;
+        return $this;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function getCep()
+    {
+        return $this->cep;
+    }
+
+    public function setCep($cep)
+    {
+        $this->cep = $cep;
+        return $this;
+    }
+
+    public function getLogradouro()
+    {
+        return $this->logradouro;
+    }
+
+    public function setLogradouro($logradouro)
+    {
+        $this->logradouro = $logradouro;
+        return $this;
+    }
+
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    public function setNumero($numero)
+    {
+        $this->numero = $numero;
+        return $this;
+    }
+
+    public function getComplemento()
+    {
+        return $this->complemento;
+    }
+
+    public function setComplemento($complemento)
+    {
+        $this->complemento = $complemento;
+        return $this;
+    }
+
+    public function getBairro()
+    {
+        return $this->bairro;
+    }
+
+    public function setBairro($bairro)
+    {
+        $this->bairro = $bairro;
+
+        return $this;
+    }
+
+    public function getCidade()
+    {
+        return $this->cidade;
+    }
+
+    public function setCidade($cidade)
+    {
+        $this->cidade = $cidade;
+
+        return $this;
+    }
+
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    public function setEstado($estado)
+    {
+        $this->estado = $estado;
+
+        return $this;
+    }
+}
