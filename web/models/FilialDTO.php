5829a3c1c7fec513afd65c4daf230c48e321febb
emg_srv4 <emgsrv4@hportugues.com.br>
2024-07-02 09:43:13 -0300

    Pronto para CR, adicionada functions no DTO

--- web/models/FilialDTO.php
+++ web/models/FilialDTO.php
@@ -4,6 +4,73 @@ abstract class FilialDTO implements DTOInterface
 {
     use DbTrait;
 
+    public static function salvar($filial)
+    {
+        $pdo = static::conectarDB();
+
+        $cepSoNumero = preg_replace('/\D/', '', $filial->getCep());
+
+        if (empty($filial->getEmpresa()->getUsuario()->getId())) {
+            if (static::verificar($filial->getCep(), $filial->getEstado())) {
+                $sql = "INSERT INTO filial (empresa_id, nome, cep, logradouro, numero, complemento, bairro, cidade, estado)
+                        VALUES (\"{$filial->getEmpresa()->getId()}\", \"{$filial->getnome()}\" ,'$cepSoNumero', \"{$filial->getLogradouro()}\", \"{$filial->getNumero()}\", \"{$filial->getEndereco()}\", \"{$filial->getComplemento()}\", \"{$filial->getBairro()}\", \"{$filial->getCidade()}\", \"{$filial->getEstado()}\")";
+            }
+        } else {
+            $sql = "UPDATE filial SET ";
+            $sql .= "nome = '{$filial->getNome()}', ";
+            $sql .= "cep = '$cepSoNumero', ";
+            $sql .= "logradouro = '{$filial->getLogradouro()}', ";
+            $sql .= "numero = '{$filial->getNumero()}', ";
+            $sql .= "complemento = '{$filial->getComplemento()}', ";
+            $sql .= "bairro = '{$filial->getBairro()}', ";
+            $sql .= "cidade = '{$filial->getCidade()}', ";
+            $sql .= "estado = '{$filial->getEstado()}', ";
+            $sql .= " WHERE empresa_id = {$filial->getEmpresa()->getId()} AND filial_id = {$filial->getId()} ";
+        }
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+
+        if (empty($filial->getId())) {
+            $filial->setId($pdo->lastInsertId());
+        }
+    }
+
+    public static function deletar($filial)
+    {
+        $pdo = static::conectarDB();
+
+        $sql = "DELETE FROM filial WHERE id = {$filial->getId()}";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+    }
+
+    public static function listar($empresa_id = '', $filial_id = '')
+    {
+        $pdo = static::conectarDB();
+
+        $sql = "SELECT * FROM filial WHERE 1 ";
+        $sql .= !empty($empresa_id) ? "AND filial.empresa_id = $empresa_id " : '';
+        $sql .= !empty($filial_id) ? "AND filial.id = $filial_id " : '';
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+
+        $retorno = [];
+        while ($filial = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $retorno[] = static::preencher($filial);
+        }
+        return $retorno;
+    }
+
+    public static function preencher($dados)
+    {
+        $empresa = EmpresaDTO::recuperar($dados['empresa_id']);
+        $filial = new Filial($empresa, $dados['nome'], $dados['cep'], $dados['logradouro'], $dados['numero'], $dados['complemento'], $dados['bairro'], $dados['cidade'], $dados['estado']);
+        $filial->setId($dados['id']);
+        return $filial;
+    }
+
     public static function verificar($cep, $estado)
     {
         $min = 8;
@@ -13,25 +80,11 @@ abstract class FilialDTO implements DTOInterface
             FlashMessage::addMessage('Numero de Cep deve conter 8 digitos', FlashMessage::FLASH_ERROR);
             header('Location: /empresa/cadastrar');
             die();
-        } elseif ($estado > $maxUf) {
-            FlashMessage::addMessage('CPF com quantidade de caracteres não permitida', FlashMessage::FLASH_ERROR);
+        } elseif (strlen($estado) != $maxUf) {
+            FlashMessage::addMessage('Numero de UF deve conter 2 digitos', FlashMessage::FLASH_ERROR);
             header('Location: /empresa/cadastrar');
             die();
-        } elseif (strlen($senha) < 8) {
-            FlashMessage::addMessage('Senha não atende ao padrão, deve ter no mínimo 8 caracteres', FlashMessage::FLASH_ERROR);
-            header('Location: /candidato/cadastrar');
-            die();
-        } else {
-            $pdo = static::conectarDB();
-
-            $sql = "SELECT COUNT(1) AS Total FROM candidato WHERE candidato.cpf LIKE '$cpf' OR candidato.email LIKE '$email'";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute();
-
-
-            $totalDeRegistros = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            return $totalDeRegistros['Total'] != 0;
         }
+        return true;
     }
 }
