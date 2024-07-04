fd17c63afcfac528a3d81e380fcf2c9907168c60
emg_srv4 <emgsrv4@hportugues.com.br>
2024-07-04 17:11:03 -0300

    atualização de dados

---
+++ web/controllers/FilialController.php
@@ -0,0 +1,83 @@
+<?php
+
+class FilialController
+{
+
+    public function __construct()
+    {
+    }
+
+    public function cadastrar()
+    {
+        AutenticacaoController::exigeSessao();
+
+        View::renderizar('filial/cadastrar', [], 'sistema');
+    }
+
+    public function processaCadastrar()
+    {
+        AutenticacaoController::renegaSessao();
+        header('Location: /autenticacao');
+        if(EmpresaDTO::verificaDadosExistentes($_POST['nome'], $_POST['cnpj']))
+        {   
+            FlashMessage::addMessage('Dados repetidos', FlashMessage::FLASH_ERROR);
+            die();
+        } 
+        
+        if (!FilialDTO::verificar($_POST['filialCep'], $_POST['filialEstado'])){
+
+            die();
+
+        }
+
+        $empresa = new Empresa($_POST['nome'], $_POST['cnpj'], $_POST['email'], $_POST['descricao'], $_POST['logo']);
+        EmpresaDTO::salvar($empresa);
+
+        $usuario = new Usuario($empresa, $_POST['usuarioCpf'], $_POST['usuarioNome'], $_POST['usuarioEmail'], password_hash($_POST['usuarioSenha'], PASSWORD_ARGON2ID));
+        UsuarioDTO::salvar($usuario);
+
+        $filial = new Filial($empresa, $_POST['filialNome'], $_POST['filialCep'], $_POST['filialLogradouro'], $_POST['filialNumero'], $_POST['filialComplemento'], $_POST['filialBairro'], $_POST['filialCidade'], $_POST['filialEstado']);
+        FilialDTO::salvar($filial);
+
+    }
+
+    public function editar()
+    {
+        AutenticacaoController::exigeSessao();
+
+        View::renderizar('empresa/editar');
+    }
+
+    public function processaEditar()
+    {
+        AutenticacaoController::exigeSessao();
+
+        $_SESSION['usuario']->getEmpresa()
+            ->setNome($_POST["nome"])
+            ->setCNPJ($_POST["cnpj"])
+            ->setEmail($_POST["email"])
+            ->setDescricao($_POST['descricao'])
+            ->setLogo($_POST['logo'])
+            ->setEndereco($_POST['endereco']);
+
+        EmpresaDTO::salvar($_SESSION['usuario']->getEmpresa());
+
+        header('Location: /vaga/listar');
+    }
+
+    public function excluir()
+    {
+        AutenticacaoController::exigeSessao();
+
+        View::renderizar('empresa/excluir');
+    }
+
+    public function processaExcluir()
+    {
+        AutenticacaoController::exigeSessao();
+
+        EmpresaDTO::deletar($_SESSION['usuario']->getEmpresa());
+
+        header('Location: /autenticacao/processaLogout');
+    }
+}
