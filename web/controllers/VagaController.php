b74dcb5e5c97a2baa81c04bfaecf692419c4f5ad
Hitman Gry <kauan.silvanascimento@outlook.com>
2024-07-05 02:08:12 -0300

    correção para CR

--- web/controllers/VagaController.php
+++ web/controllers/VagaController.php
@@ -38,7 +38,6 @@ class VagaController
 
         $filial = FilialDTO::recuperar($_POST['filial']);
 
-        
         if (empty($_POST['vagaId'])) {
             $vaga = new Vaga($filial,$_SESSION['usuario']->getEmpresa(), $_POST['titulo'], $_POST['email'], $_POST['salario'], $_POST['beneficios'], $_POST['descricao'], $_POST['requisitos'], $_POST['cargaHoraria'], $_POST['regimeContratacao'], $_POST['regimeTrabalho'], $_POST['nivelSenioridade'], $_POST['nivelHierarquia'], $_POST['status']);
         } else {
--- web/models/EmpresaDTO.php
+++ web/models/EmpresaDTO.php
@@ -58,6 +58,7 @@ abstract class EmpresaDTO implements DTOInterface
     {
         $pdo = static::conectarDB();
         $sql = "SELECT * FROM empresa WHERE id = $id ";
+        
         $stmt = $pdo->prepare($sql);
         $stmt->execute();
 
