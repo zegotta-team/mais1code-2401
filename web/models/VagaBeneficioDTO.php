3add137c14b78e30fb1b6063a72d299a71889202
Edyelgue Carneiro <edyelgue12@gmail.com>
2024-07-21 05:42:32 -0300

    criado DTOs para beneficio e vaga beneficio

---
+++ web/models/VagaBeneficioDTO.php
@@ -0,0 +1,86 @@
+<?php
+
+abstract class VagaBeneficioDTO implements DTOInterface
+{
+    use DbTrait;
+
+    public static function salvar($vagaBeneficio){
+        $pdo = static::conectarDB();
+
+        if (empty($vagaBeneficio->getVagaId()) && empty($vagaBeneficio->getBeneficioId())) {
+            $sql = "INSERT INTO vaga_beneficio (vaga_id, beneficio_id, informacao)
+                    VALUES ({$vagaBeneficio->getVaga()->getId()}, 
+                            {$vagaBeneficio->getBeneficio()->getId()}, 
+                            \"{$vagaBeneficio->getInformacao()}\")";
+        } else {
+            $sql = "UPDATE vaga_beneficio SET ";
+            $sql .= "informacao = '{$vagaBeneficio->getInformacao()}' ";
+            $sql .= "WHERE id = {$vagaBeneficio->getVaga()->getId()} ";
+            $sql .= "AND id = {$vagaBeneficio->getBeneficio()->getId()} ";
+        }
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+    }
+
+    public static function deletar($vagaBeneficio)
+    {
+        $pdo = static::conectarDB();
+
+        $sql = "DELETE FROM vaga_beneficio ";
+        $sql .= "WHERE vaga_id = {$vagaBeneficio->getVagaId()} ";
+        $sql .= "AND beneficio_id = {$vagaBeneficio->getBeneficioId()} ";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+    }
+
+    public static function recuperar($vagaId, $beneficioId = ''){
+        if (empty($beneficioId)) {
+            return null;
+        }
+
+        $pdo = static::conectarDB();
+        $sql = "SELECT * FROM vaga_beneficio ";
+        $sql .= "WHERE vaga_id = $vagaId ";
+        $sql .= "AND beneficio_id = $beneficioId ";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+
+        $retorno = null;
+        
+        while ($vagaBeneficio = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $retorno = static::preencher($vagaBeneficio);
+        }
+
+        return $retorno;
+    }
+
+    public static function listar(){
+        $pdo = static::conectarDB();
+
+        $sql = "SELECT * ";
+        $sql .= "FROM vaga_beneficio ";
+        $sql .= "LEFT JOIN beneficios b ";
+        $sql .= "ON b.id = beneficio_id";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+
+        $retorno = [];
+        while($beneficio = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $retorno[] = static::preencher($beneficio);
+        }
+
+        return $retorno;
+    }
+
+    public static function preencher($dados){
+        $vagaId = VagaDTO::recuperar($dados['id']);
+        $beneficioId = BeneficioDTO::recuperar($dados['id']);
+        $vagaBeneficio = new VagaBeneficio($vagaId, $beneficioId, $dados['informacao']);
+
+        return $vagaBeneficio;
+    }
+}
